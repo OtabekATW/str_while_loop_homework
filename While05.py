0f238f0a35990c1c8a4e35d39fb0ffc7b4576286
OtabekATW <otabektursunov468@gmail.com>
2023-11-22 09:33:56 +0500

    While05.py

--- While05.py
+++ While05.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    k = 0
+    lower = ''
+    while i < len(s):
+        if s[i].islower():
+            lower += s[i]
+            k += 1
+        i += 1
+    return k
