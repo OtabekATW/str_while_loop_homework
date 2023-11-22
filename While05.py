0f829c1d526c005af1b03e09cd27f888dc2bb7d9
OtabekATW <otabektursunov468@gmail.com>
2023-11-22 09:40:58 +0500

    While05.py

--- While05.py
+++ While05.py
@@ -11,7 +11,7 @@ def main(s):
     lower = ''
     while i < len(s):
         if s[i].islower():
-            lower += s[i]
+            #lower += s[i]
             k += 1
         i += 1
-    return k
+    return k #len(lower) 
